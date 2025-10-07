f831abff02402ea993dfa6c8c4c751efdd3a78ff
nati-tewolde <natisnewemail@gmail.com>
2025-10-07 14:19:00 -0400

    formatted dates using ZonedDateTime & .withZoneSameInstant

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,7 +1,22 @@
 package com.pluralsight;
 
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
     public static void main(String[] args) {
+
+        ZonedDateTime currentDateTime = ZonedDateTime.now();
+        ZonedDateTime gmtDateTime = ZonedDateTime.now().withZoneSameInstant(ZoneId.of("GMT"));
+
+        System.out.println("Date Formats\n===================");
+        System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+        System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("MMMM d, yyyy")));
+        System.out.println(gmtDateTime.format(DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy hh:mm")));
+        System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("h:mm 'on' dd-MMM-yyyy")));
 
     }
 }
