92152fe19f64adee2e3f2e5c4eb8798a63d00529
nati-tewolde <natisnewemail@gmail.com>
2025-10-07 22:31:53 -0400

    added LocalDateTime object

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,22 +1,23 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FormatDates {
     public static void main(String[] args) {
 
-        ZonedDateTime currentDateTime = ZonedDateTime.now();
-        ZonedDateTime gmtDateTime = ZonedDateTime.now().withZoneSameInstant(ZoneId.of("GMT"));
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        ZonedDateTime currentZonedDateTime = ZonedDateTime.now();
+        ZonedDateTime gmtDateTime = currentZonedDateTime.withZoneSameInstant(ZoneId.of("GMT"));
 
-        System.out.println("Date Formats\n===================");
+        System.out.println("Date Formats\n========================");
         System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
         System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
         System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("MMMM d, yyyy")));
-        System.out.println(gmtDateTime.format(DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy hh:mm")));
-        System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("h:mm 'on' dd-MMM-yyyy")));
+        System.out.println(gmtDateTime.format(DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy HH:mm '(in GMT)'")));
+        System.out.println(currentDateTime.format(DateTimeFormatter.ofPattern("H:mm 'on' dd-MMM-yyyy '(in local time zone)'")));
 
     }
 }
